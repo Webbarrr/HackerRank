1a06fd257c1a2d2b7a2786d72b669cdf96adbc0d
James Webber <James.Webber@cansfordlabs.co.uk>
2019-07-12 20:59:00 +0100

    utopian tree

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HackerRank
 {
@@ -10,6 +6,31 @@ namespace HackerRank
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(UtopianTree(0));
+            Console.WriteLine(UtopianTree(1));
+            Console.WriteLine(UtopianTree(4));
+        }
+
+        public static int UtopianTree(int growthCycles)
+        {
+            const int HEIGHT = 1;
+
+            // no need to do anything
+            if (growthCycles == 0)
+                return HEIGHT;
+
+            int retVal = HEIGHT;
+
+            // iterate through numbers & get the new height
+            for (int i = 1; i <= growthCycles; i++)
+            {
+                if (i % 2 == 1)
+                    retVal *= 2;
+                else
+                    retVal++;
+            }
+
+            return retVal;
         }
     }
 }
