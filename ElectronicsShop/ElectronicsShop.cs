d1f2f244c975062acfddb14082de2c31c9f5a5ef
James Webber <James.Webber@cansfordlabs.co.uk>
2019-07-14 09:39:54 +0100

    made ElectronicsShop object orientated & acted on feedback from https://codereview.stackexchange.com/questions/224100/hacker-rank-electronics-shop

---
+++ ElectronicsShop/ElectronicsShop.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ElectronicsShop
+{
+    public class ElectronicsShop
+    {
+        private int _budget;
+
+        public int GetMoneySpent(int[] keyboardPrices, int[] drivePrices, int budget)
+        {
+            if (keyboardPrices == null)
+                throw new ArgumentNullException(nameof(keyboardPrices));
+
+            if (drivePrices == null)
+                throw new ArgumentNullException(nameof(drivePrices));
+
+            if (budget < 0)
+                throw new ArgumentOutOfRangeException(nameof(budget), "Budget must be non-negative");
+
+            this._budget = budget;
+
+            // filter out items that are over budget & sort
+            var affordableKeyboardPrices = GetAffordableItems(keyboardPrices);
+            var affordableDrivePrices = GetAffordableItems(drivePrices);
+
+            // create a new list of valid combinations
+            var combinedTotals = new List<int>();
+
+            foreach (var affordableKeyboardPrice in affordableKeyboardPrices)
+            {
+                foreach (var affordableDrivePrice in affordableDrivePrices)
+                {
+                    var price = affordableKeyboardPrice + affordableDrivePrice;
+
+                    if (price <= this._budget)
+                        combinedTotals.Add(price);
+                }
+            }
+
+            return combinedTotals.Count == 0 ? -1 : combinedTotals[0];
+        }
+
+        private IEnumerable<int> GetAffordableItems(int[] array)
+        {
+            return array.OrderBy(n => n).TakeWhile(n => n <= this._budget).Reverse();
+        }
+    }
+}
--- ElectronicsShop/Program.cs
+++ ElectronicsShop/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace ElectronicsShop
 {
@@ -8,55 +6,11 @@ namespace ElectronicsShop
     {
         static void Main(string[] args)
         {
+            var electronicsShop = new ElectronicsShop();
 
-            Console.WriteLine(GetMoneySpent(new int[] { 3, 1 }, new int[] { 5, 2, 8 }, 10));
-            Console.WriteLine(GetMoneySpent(new int[] { 5}, new int[] { 4 }, 5));
+            Console.WriteLine(electronicsShop.GetMoneySpent(new int[] { 3, 1 }, new int[] { 5, 2, 8 }, 10));
+            Console.WriteLine(electronicsShop.GetMoneySpent(new int[] { 5 }, new int[] { 4 }, 5));
             Console.ReadLine();
-        }
-
-        static int GetMoneySpent(int[] keyboards, int[] drives, int budget)
-        {
-            if (budget == 0)
-                return -1;
-
-            // sort the two arrays so the highest values are at the front
-            keyboards = SortArrayDescending(keyboards);
-            drives = SortArrayDescending(drives);
-
-            // delete any that are over our budget
-            var affordableKeyboards = GetAffordableItems(keyboards, budget);
-            var affordableDrives = GetAffordableItems(drives, budget);
-
-            // make a list to contain the combined totals
-            var combinedTotals = new List<int>();
-
-            foreach (var keyboard in keyboards)
-            {
-                foreach (var drive in drives)
-                {
-                    combinedTotals.Add(keyboard + drive);
-                }
-            }
-
-            // sort the list & delete anything over budget
-            combinedTotals.Sort();
-            combinedTotals.Reverse();
-            combinedTotals.RemoveAll(n => n > budget);
-
-            return combinedTotals.Count == 0 ? -1 : combinedTotals[0];
-        }
-
-        static int[] SortArrayDescending(int[] array)
-        {
-            Array.Sort(array);
-            Array.Reverse(array);
-
-            return array;
-        }
-
-        static int[] GetAffordableItems(int[] array, int budget)
-        {
-            return array.Where(n => n < budget).ToArray();
         }
     }
 }
