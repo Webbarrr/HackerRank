e30c0ab4dbef4b82572d011333b6c7da0334d4f4
James Webber <James.Webber@cansfordlabs.co.uk>
2019-07-21 19:43:01 +0100

    completed broken records

---
+++ Challenges/BreakingRecords/CalculateBrokenRecords.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges.BreakingRecords
+{
+    public class CalculateBrokenRecords
+    {
+        public int[] GetBrokenRecords(int[] scores)
+        {
+            if (scores == null)
+                throw new ArgumentNullException(nameof(scores));
+
+            var minimumScore = scores[0];
+            var maximumScore = scores[0];
+            var timesBeaten = new int[] { 0, 0 };
+
+            var newScores = scores.Skip(1).Take(scores.Length);
+
+            foreach (var score in newScores)
+            {
+                if (score > minimumScore && score < maximumScore)
+                    continue;
+
+                if (score < minimumScore)
+                {
+                    minimumScore = score;
+                    timesBeaten[1]++;
+                }
+                else if (score > maximumScore)
+                {
+                    maximumScore = score;
+                    timesBeaten[0]++;
+                }
+            }
+
+            return timesBeaten;
+        }
+    }
+}
--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -1,5 +1,5 @@
-﻿using ApplesAndOranges;
-using System;
+﻿using System;
+using Challenges.BreakingRecords;
 
 namespace Challenges
 {
@@ -7,7 +7,24 @@ namespace Challenges
     {
         static void Main(string[] args)
         {
+            var recordCalculator = new CalculateBrokenRecords();
 
+            var scores = new int[] {12, 24, 10, 24};
+
+            var recordsBroken = recordCalculator.GetBrokenRecords(scores);
+
+            var result = string.Join(" ", recordsBroken);
+
+            Console.WriteLine(result); // expected "1 1"
+
+            scores = new int[] { 3, 4, 21, 36, 10, 28, 35, 5, 24, 42 };
+
+            recordsBroken = recordCalculator.GetBrokenRecords(scores);
+
+            result = string.Join(" ", recordsBroken);
+
+            Console.WriteLine(result); // expected "4 0"
+            Console.ReadLine();
         }
     }
 }
