a2685abe780c0c61f1e75179d9f13aeef42b3cfe
James Webber <James.Webber@cansfordlabs.co.uk>
2019-07-21 12:17:19 +0100

    Added units tests for BetweenTwoSets

---
+++ BetweenTwoSets.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BetweenTwoSets.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("BetweenTwoSets.Tests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("89e519bf-8ad4-4f78-ab65-37ae04ff1f0c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BetweenTwoSets.Tests/SetLogicTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace BetweenTwoSets.Tests
+{
+    [TestFixture]
+    public class SetLogicTests
+    {
+        private SetLogic _setLogic;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _setLogic = new SetLogic();
+        }
+
+        [Test]
+        public void GetTotalX_ValidLists_Returns2()
+        {
+            var a = new List<int> {2, 6};
+            var b = new List<int> { 24, 36 };
+
+            var result = _setLogic.GetTotalX(a, b);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void GetTotalX_InvalidLists_ThrowsException()
+        {
+            Assert.That(() => _setLogic.GetTotalX(null, null),Throws.ArgumentNullException);
+        }
+    }
+}
