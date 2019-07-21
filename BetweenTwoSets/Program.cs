6f5c7d2fe304003a3d2a0ae6b2434b6774aa418d
James Webber <James.Webber@cansfordlabs.co.uk>
2019-07-21 12:17:02 +0100

    completed BetweenTwoSets

--- BetweenTwoSets/Program.cs
+++ BetweenTwoSets/Program.cs
@@ -1,62 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BetweenTwoSets
 {
-    class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
             try
             {
-                GetTotalX(new List<int>
-                {
-                    2,
-                    4
-                }, new List<int>
-                {
-                    16,
-                    32,
-                    96
-                });
+                var a = new List<int> {2, 6};
+                var b = new List<int> {24, 36};
+
+                var setLogic = new SetLogic();
+                var count = setLogic.GetTotalX(a, b);
+                Console.WriteLine(count); // expecting 2
+
+                Console.ReadLine();
 
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
             }
-        }
-
-        public static int GetTotalX(List<int> list1, List<int> list2)
-        {
-            if (list1 == null)
-                throw new ArgumentNullException(nameof(list1));
-
-            if (list2 == null)
-                throw new ArgumentNullException(nameof(list2));
-
-            // something to hold our valid divisors
-            var validDivisors = new List<int>();
-
-            // find the upper & lower elements from the two lists
-            var lowerElement = list1[list1.Count - 1];
-            var upperElement = list2[0];
-
-            for (var i = lowerElement; i < upperElement; i++)
-            {
-                foreach (var item in list1)
-                {
-                    if (i % item != 0)
-                    {
-
-                    }
-                } 
-            }
-
-            return 0;
         }
     }
 }
---
+++ BetweenTwoSets/SetLogic.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BetweenTwoSets
+{
+    public class SetLogic
+    {
+        private List<int> _a;
+        private List<int> _b;
+        private int _incrementor;
+
+        private readonly List<int> _potentialDivisors;
+
+        public readonly List<int> ValidDivisors;
+
+        public SetLogic()
+        {
+            ValidDivisors = new List<int>();
+            _potentialDivisors = new List<int>();
+        }
+
+        public int GetTotalX(List<int> a, List<int> b)
+        {
+            this._a = a ?? throw new ArgumentNullException(nameof(a));
+            this._b = b ?? throw new ArgumentNullException(nameof(b));
+
+            // get the lower & upper boundaries from the given sets
+            var lowerElement = a[a.Count - 1];
+            var upperElement = b[0];
+
+            _incrementor = (this.AreAllElementsEven() ? 2 : 1);
+
+            bool failCondition;
+
+            // find potential divisors
+            for (int i = lowerElement; i <= upperElement; i+=_incrementor)
+            {
+                failCondition = false;
+
+                foreach (var item in a)
+                {
+                    if (!(i % item == 0))
+                    {
+                        failCondition = true;
+                        break;
+                    }
+                }
+
+                if (!failCondition)
+                    foreach (var item in b)
+                    {
+                        if (!(item % i == 0))
+                        {
+                            failCondition = true;
+                            break;
+                        }
+                    }
+
+                if (!failCondition)
+                    ValidDivisors.Add(i);
+            }
+
+            return ValidDivisors.Count;
+        }
+
+        private bool ConsiderXInY(int x, List<int> y)
+        {
+            foreach (var i in y)
+            {
+                if (!(x % i == 0))
+                    return false;
+            }
+
+            return true;
+        }
+
+        private bool AreAllElementsEven()
+        {
+
+            if (this._a.Where(n => n % 2 == 0).ToArray().Length == this._a.Count 
+                && this._b.Where(n => n % 2 == 0).ToArray().Length == this._b.Count)
+                return true;
+
+            return false;
+        }
+    }
+}
