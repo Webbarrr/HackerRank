f5c979814e38ec2c5f778f7e7dfb68e070bdca4a
James Webber <James.Webber@cansfordlabs.co.uk>
2019-07-26 21:02:38 +0100

    started DayOfProgrammer

--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using Challenges.MigratoryBirds;
 
 namespace Challenges
 {
@@ -8,32 +6,16 @@ namespace Challenges
     {
         static void Main(string[] args)
         {
-            // test 1
-            var result = MigratoryBirds(new List<int> {1, 1, 2, 2, 3});
-            Console.WriteLine(result); // expecting 1
 
-            // test 2
-            result = MigratoryBirds(new List<int> { 3,2,2,1,1 });
-            Console.WriteLine(result); // expecting 1
-
-            // test 3
-            result = MigratoryBirds(new List<int> {1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4});
-            Console.WriteLine(result); // expecting 3
 
             Console.ReadLine();
         }
 
-        static int MigratoryBirds(List<int> arr)
+        static string DayOfProgrammer(int year)
         {
-            var birds = new List<Bird>();
 
-            foreach (var bird in arr)
-            {
-                birds.Add(new Bird{Id = bird});
-            }
 
-            var birdWatcher = new BirdWatcher();
-            return birdWatcher.MostCommonBird(birds);
+            return null;
         }
     }
 }
