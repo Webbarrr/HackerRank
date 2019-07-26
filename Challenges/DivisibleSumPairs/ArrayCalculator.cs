7b25e17dac00c499db352da29b6539a82383e11c
James Webber <James.Webber@cansfordlabs.co.uk>
2019-07-26 19:55:36 +0100

    corrected mistake in logic - completed DivisibleSumPairs

--- Challenges/DivisibleSumPairs/ArrayCalculator.cs
+++ Challenges/DivisibleSumPairs/ArrayCalculator.cs
@@ -24,16 +24,13 @@ namespace Challenges.DivisibleSumPairs
 
             var x = ints.Length - 1;
 
-            for (int i = 0; i < x; i++)
+            for (int i = 0; i <= x; i++)
             {
-                for (int j = i + 1; j < x; j++)
+                for (int j = i + 1; j <= x; j++)
                 {
-                    if (ints[i] < ints[j])
-                    {
-                        var sum = ints[i] + ints[j];
-                        if (sum % divisibleBy == 0)
-                            this._sumPairs.Add(new int[]{ints[i], ints[j]});
-                    }
+                    var sum = ints[i] + ints[j];
+                    if (sum % divisibleBy == 0)
+                        this._sumPairs.Add(new int[]{ints[i], ints[j]});
                 }
             }
 
--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -11,6 +11,7 @@ namespace Challenges
             var result = DivisibleSumPairs(6, 5, new[] {1, 2, 3, 4, 5, 6});
             Console.WriteLine(result); // expecting 3
 
+            // test 2
             result = DivisibleSumPairs(6, 3, new[] {1, 3, 2, 6, 1, 2});
             Console.WriteLine(result); // expecting 5
 
