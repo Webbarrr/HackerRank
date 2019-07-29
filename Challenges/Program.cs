6932806207fbaa8f370f33f277fa252c9b59c3d5
James Webber <James.Webber@cansfordlabs.co.uk>
2019-07-29 12:06:55 +0100

    started BonAppetit

--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Challenges.DayOfTheProgrammer;
+using System.Collections.Generic;
 
 namespace Challenges
 {
@@ -7,45 +7,15 @@ namespace Challenges
     {
         static void Main(string[] args)
         {
-            // test 1
-            var result = DayOfProgrammer(1984);
-            Console.WriteLine(result); // 12.09.1984
 
-            // test 2
-            result = DayOfProgrammer(2017);
-            Console.WriteLine(result); // 13.09.2017
-
-            // test 3
-            result = DayOfProgrammer(2016);
-            Console.WriteLine(result); // 12.09.2016
-
-            // test 4
-            result = DayOfProgrammer(1800);
-            Console.WriteLine(result); // 12.09.1800
-
-            // test 5
-            result = DayOfProgrammer(1801);
-            Console.WriteLine(result); // 13.09.1801
-
-            // test 6
-            result = DayOfProgrammer(1916);
-            Console.WriteLine(result); // 12.09.1916
-
-            // test 7
-            result = DayOfProgrammer(1836);
-            Console.WriteLine(result); // 12.09.1836
-
-            // test 8
-            result = DayOfProgrammer(1918);
-            Console.WriteLine(result); // 26.09.1918
 
             Console.ReadLine();
         }
 
-        static string DayOfProgrammer(int year)
+        static void BonAppetit(List<int> bill, int k, int b)
         {
-            var timeMachine = new TimeMachine(year);
-            return timeMachine.GetDayOfTheProgrammer().ToString("dd.MM.yyyy");
+
+
         }
     }
 }
