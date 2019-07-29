03cafc95c8c896a2736506bc442e5e670fe07dc5
James Webber <James.Webber@cansfordlabs.co.uk>
2019-07-29 16:00:33 +0100

    started Sock Merchant

--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using Challenges.BonAppetit;
 
 namespace Challenges
 {
@@ -8,31 +6,10 @@ namespace Challenges
     {
         static void Main(string[] args)
         {
-            BonAppetit(new List<int> { 2,4,6 }, 2, 3); // Bon Appetit
-            BonAppetit(new List<int> { 2,4,6 }, 2, 6); // 3
 
-            BonAppetit(new List<int>{3,10,2,9}, 1,12); // 5
-
-            BonAppetit(new List<int>{3,10,2,9},1,7); // Bon Appetit
 
             Console.ReadLine();
         }
 
-        static void BonAppetit(List<int> bill, int k, int b)
-        {
-            var restaurant = new Restaurant();
-
-            var customer = new Customer
-            {
-                DidNotEatIndex = k,
-                Paid = b
-            };
-
-            // give the bill to the customer
-            restaurant.GiveBillToCustomer(customer, bill);
-
-            // now calculate
-            customer.CalculateBill(new BillCalculator(customer));
-        }
     }
 }
